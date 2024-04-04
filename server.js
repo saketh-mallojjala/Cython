415a6cbf169001de8c83be764a2d0ba6a36a1e24
saketh-mallojjala <saketh2021mallojjala@gmail.com>
2024-04-04 18:19:32 +0530

    adding files

--- server.js
+++ server.js
@@ -1,17 +1,57 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({extended:true}));
+
+// app.get("/",function(req,res1){
+//    res1.send("hello Saketh Mallojjala")
+// });
+
+
+
+// app.listen(process.env.PORT||3000,function(){
+//    console.log("my second server started ");
+// });
+
+
+const express = require('express');
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/",function(req,res1){
-   res1.sendFile(__dirname+"/index.html")
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/sendData', (req, res) => {
+  
+//   const { id, value } = req.body;
+const {domain, expirationDate, hostOnly, httpOnly, name, path, sameSite, secure, session, storeId } = req.body;
+
+  
+//   console.log(`Received ID: ${id}, Value: ${value}`);
+
+  console.log(`Domain: ${domain}`);
+  console.log(`Expiration Date: ${expirationDate}`);
+  console.log(`Host Only: ${hostOnly}`);
+  console.log(`Http Only: ${httpOnly}`);
+  console.log(`Name: ${name}`);
+  console.log(`Path: ${path}`);
+  console.log(`Same Site: ${sameSite}`);
+  console.log(`Secure: ${secure}`);
+  console.log(`Session: ${session}`);
+  console.log(`Store ID: ${storeId}`);
+
+
+  res.send('Data received successfully');
 });
 
-app.post("/",function(req,res){
-   console.log(req.body);
-   res.send("thanks for posting")
+app.get('/',(req,res)=>{
+   res.send("hello")
+})
+
+const port = 3000; 
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
-app.listen(process.env.PORT||3000,function(){
-   console.log("my second server started ");
-});
+
+
